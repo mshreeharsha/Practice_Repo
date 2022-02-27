e920fb948f56c42aa65c1201a212b7fb77197972
Pulkit002 <97612856+Pulkit002@users.noreply.github.com>
2022-02-27 10:34:19 +0530

    added new file pulkit.cpp

---
+++ Pulkit_211ch045/pulkit.cpp
@@ -0,0 +1,11 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+int main(){
+    string s;
+    cout<<"What is your name?";
+    cin>>s;
+    cout<<"Hello "<<s<<endl;
+    cout<<"my name is Pulkit Gupta";
+}
