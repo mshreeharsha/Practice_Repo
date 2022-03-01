ffc65cf52f932f2d6facec554ea001aa17e8aba6
mshreeharsha <shreeharsha.mbhat@gmail.com>
2022-03-01 23:08:37 +0530

    My First Commit for First Code

---
+++ mshreeharsha_211CS137/myfirstgitcode.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main(){
+    int a,b,sum;
+    printf("\nHello Everyone!");
+    printf("\n\n Adding 2 Numbers. Enter 2 Numbers:  ");
+    scanf("%d%d",&a,&b);
+    sum=a+b;
+    printf("\nSum of %d and %d is : %d",a,b,sum);
+    return 0;
+} 
