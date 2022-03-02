65d8945593b16932525c67d61b643b3acb3350dc
mshreeharsha <shreeharsha.mbhat@gmail.com>
2022-03-02 23:11:26 +0530

    My first commit

---
+++ mshreeharsha_211CS137/FirstCode.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+int main(){
+    int a,b,c;
+    printf("\nEnter the 2 Numbers : ");
+    scanf("%d%d",&a,&b);
+    c=a+b;
+    printf("Sum of 2 Numbers %d and %d is : %d",a,b,c);
+}
